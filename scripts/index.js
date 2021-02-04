0242c3dd56b4457c0a7c70e98477252c68ee8499
Scal3 <cool.goga501@gmail.com>
2021-02-04 14:15:20 +0300

    ver1.5.1

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,14 @@ const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-const popupSubmitButton = document.querySelector('.popup__submit');
 let typeName = document.querySelector('.popup__input_type_name');
 let typeJob = document.querySelector('.popup__input_type_job');
 const popupForm = document.querySelector('.popup__form');
 
 
 function openPopupWindow(){
-    typeName.value = profileName.innerHTML;
-    typeJob.value = profileJob.innerHTML;
+    typeName.value = profileName.textContent;
+    typeJob.value = profileJob.textContent;
     popup.classList.add('popup_opened');
 }
 
